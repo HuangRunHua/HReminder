7867fd05b460959a930ca9f5cfbcc0f8c046ca78
HuangRunHua <h76eddy@icloud.com>
2019-07-07 13:54:31 +0800

    Initial Commit

---
+++ HReminder/HReminder.h
@@ -0,0 +1,19 @@
+//
+//  HReminder.h
+//  HReminder
+//
+//  Created by Joker Hook on 2019/7/7.
+//  Copyright © 2019 Joker Hook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HReminder.
+FOUNDATION_EXPORT double HReminderVersionNumber;
+
+//! Project version string for HReminder.
+FOUNDATION_EXPORT const unsigned char HReminderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HReminder/PublicHeader.h>
+
+
